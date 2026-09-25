40b141ded99b37560b4610178bfdd1e3cc4baecb
Celsogit <celsocartaya2019@gmail.com>
2026-09-25 17:23:48 -0600

    Fix: revert to base '/gamerzone-n/' + no basepath + hash routing (prev config worked in run #14)

--- src/router.tsx
+++ src/router.tsx
@@ -13,7 +13,6 @@ export const getRouter = () => {
   const router = createRouter({
     routeTree,
     history: hashHistory,
-    basepath: "/gamerzone-n",
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
